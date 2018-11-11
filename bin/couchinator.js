edd10965243cbde99c765991d5bca51680a9c5a1
Carmine DiMascio <dimascio@us.ibm.com>
2018-11-11 12:17:25 -0500

    returned failed exit status on error

--- bin/couchinator.js
+++ bin/couchinator.js
@@ -1,62 +1,56 @@
 #!/usr/bin/env node
- 
+
 const program = require('commander');
 const path = require('path');
 const Generator = require('../lib');
-const package = require('../package.json')
+const package = require('../package.json');
 const DEFAULT_PATH = 'cloudant-database';
 const required = (val, name) => {
   if (!val) {
     console.error(`${name} required.`);
     process.exit(1);
   }
-}
+};
 let command;
-program
-  .version(package.version)
+program.version(package.version);
 
-program
-  .command('create')
-  .action(cmd => command = 'create')
+program.command('create').action(cmd => (command = 'create'));
 
-program
-  .command('destroy')
-  .action(cmd => command = 'destroy')
+program.command('destroy').action(cmd => (command = 'destroy'));
 
-program
-  .command('recreate')
-  .action(cmd => command = 'recreate')
+program.command('recreate').action(cmd => (command = 'recreate'));
 
 program
   .option('-u --url <url>', 'couchdb url')
   .option('-p --path <path>', 'resource path. Default ./' + DEFAULT_PATH)
   .option('-b --verbose', 'verbose logs', false)
-  .option('-d --ddocsonly', 'import design docs only. Do no import other docs ', false)
-
+  .option(
+    '-d --ddocsonly',
+    'import design docs only. Do no import other docs ',
+    false
+  );
 
 program.parse(process.argv);
 
 validate();
 
-const rpath = program.path 
-  ? path.isAbsolute(program.path) 
-    ? program.path 
-    : path.join(process.cwd(), program.path) 
+const rpath = program.path
+  ? path.isAbsolute(program.path)
+    ? program.path
+    : path.join(process.cwd(), program.path)
   : path.join(process.cwd(), DEFAULT_PATH);
 
-
-console.log
+console.log;
 main({
   command,
   url: program.url,
   path: rpath,
   allDocs: !program.designonly,
-  verbose: program.verbose
+  verbose: program.verbose,
 });
 
-
 function main(opts) {
-  const command = opts.command
+  const command = opts.command;
   const url = opts.url;
   const rpath = opts.path;
   const allDocs = opts.allDocs;
@@ -70,18 +64,27 @@ function main(opts) {
         console.log(o.msg);
       }
     }
-  } 
+  };
   const generator = new Generator(url, visitor);
 
-  switch(command) {
+  switch (command) {
     case 'create':
-      generator.resources(rpath).create(allDocs);
-      break;  
+      generator
+        .resources(rpath)
+        .create(allDocs)
+        .catch(e => process.exit(2));
+      break;
     case 'destroy':
-      generator.resources(rpath).destroy()
+      generator
+        .resources(rpath)
+        .destroy()
+        .catch(e => process.exit(2));
       break;
     case 'recreate':
-      generator.resources(rpath).recreate(allDocs);
+      generator
+        .resources(rpath)
+        .recreate(allDocs)
+        .catch(e => process.exit(2));
       break;
     default:
       exit('Invalid command.');
@@ -89,7 +92,7 @@ function main(opts) {
 }
 
 function validate() {
-  if (program.args.length === 0) exit()
+  if (program.args.length === 0) exit();
   if (!command) exit('Invalid command.');
   if (!program.url) exit('Url required.');
   if (program.args.length === 0) exit();
@@ -97,8 +100,8 @@ function validate() {
 
 function exit(msg) {
   if (msg) {
-    console.error('  Error: '+msg)
+    console.error('  Error: ' + msg);
   }
   program.help();
-  process.exit(1)
+  process.exit(1);
 }
